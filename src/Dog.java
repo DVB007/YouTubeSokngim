30ab46b1c45d16f04b8afb59dd0de0723f881468
devid.phin <phinsopheacheadavid@gmail.com>
2026-05-10 14:32:07 +0700

    Blueprint

---
+++ src/Dog.java
@@ -0,0 +1,17 @@
+public class Dog {
+    //State => Name Age Color
+    public String name = "Ki Ki";
+    public int age = 2;
+    public String color = "Black";
+
+    //Behavior = Bark Eat Sleep
+    public void Bark(){
+        System.out.println("Vos Vos Vos");
+    }
+    public void Eat(){
+        System.out.println("Meat");
+    }
+    public void Sleep(){
+        System.out.println("ZZZ");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
 public class Main {
+    public static void main(String[] args) {
+
+        //To create Object from Class Dog
+        Dog dog1 = new Dog();
+        dog1.Bark();
+        dog1.Eat();
+        dog1.Sleep();
+        System.out.println("Name : "+dog1.name);
+        System.out.println("Age : "+dog1.age);
+        System.out.println("Color : "+dog1.color);
+    }
 }
